664b3b5b7e83f4c30c45d1417253584e4fc7ee93
giricse <kemburugiriprasad72@gmail.com>
2020-02-08 00:49:22 +0530

    First commit

---
+++ MavenJava/src/test/java/MyFirstProject/RESTAPITest.java
@@ -0,0 +1,13 @@
+package MyFirstProject;
+
+import org.testng.annotations.Test;
+
+public class RESTAPITest {
+	
+	@Test
+	public void restapitest1()
+	{
+		System.out.println("This is Rest API Test1");
+	}
+
+}
---
+++ MavenJava/src/test/java/MyFirstProject/SeleniumTest.java
@@ -0,0 +1,13 @@
+package MyFirstProject;
+
+import org.testng.annotations.Test;
+
+public class SeleniumTest {
+	
+	@Test
+	public void firsttest()
+	{
+		System.out.println("This is First");
+	}
+
+}
